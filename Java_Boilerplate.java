0b2366a9ede4e1e02918ac11e6ba9468430a8fcf
shazzadhsn <shazzadmail1@gmail.com>
2025-11-04 21:08:58 +0600

    first-commit

---
+++ Java_Boilerplate.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Java_Boilerplate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
